66ba0734e80715c0b94e47047deca5ae723e71c0
cheeseong <cheeseong.thrivetechsolution@gmail.com>
2022-04-06 14:11:22 +0800

    initial commit

---
+++ config/console.php
@@ -0,0 +1,55 @@
+<?php
+
+$params = require __DIR__ . '/params.php';
+if (YII_ENV == 'dev')   $db = require __DIR__ . '/db_stage.php';
+if (YII_ENV == 'prod')  $db = require __DIR__ . '/db_prod.php';
+
+$config = [
+    'id' => 'basic-console',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'app\commands',
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+        '@tests' => '@app/tests',
+    ],
+    'components' => [
+		'redis' => [
+			'class' => 'yii\redis\Connection',
+			'hostname' => 'localhost',
+			'port' => 6379,
+			'database' => 0,
+		],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+    ],
+    'params' => $params,
+    /*
+    'controllerMap' => [
+        'fixture' => [ // Fixture generation command line.
+            'class' => 'yii\faker\FixtureController',
+        ],
+    ],
+    */
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
---
+++ config/params.php
@@ -0,0 +1,7 @@
+<?php
+Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
+return [
+    'adminEmail' => 'admin@example.com',
+    'senderEmail' => 'noreply@example.com',
+    'senderName' => 'Example.com mailer',
+];
