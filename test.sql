3bd81d33ecc829f7c94486db64781ce7f2c4ffc9
cheeseong <cheeseong.thrivetechsolution@gmail.com>
2022-04-11 00:00:54 +0800

    sql

--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 08, 2022 at 11:58 AM
+-- Generation Time: Apr 10, 2022 at 06:00 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.28
 
@@ -55,6 +55,7 @@ CREATE TABLE `user` (
   `username` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   `salt` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(63) CHARACTER SET utf8 DEFAULT NULL,
   `email` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
   `dob` date DEFAULT NULL,
   `gender` int(1) NOT NULL DEFAULT 1,
@@ -77,8 +78,8 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `department_id`, `username`, `password`, `salt`, `email`, `dob`, `gender`, `avatar`, `sound`, `phone`, `remark`, `status`, `last_login`, `login_ip`, `fail_login`, `created_at`, `create_by`, `updated_at`, `update_by`, `access_token`) VALUES
-(1, NULL, 'cheeseong', 'a434df8389d477852e159ab794d9b637cfdbc19aefe896c887956b1214dcd1e9', 'zhCK7nORKh8Ou0uOI7K69pz28hbMjI33', 'cheeseong.thrivetechsolution@gmail.com', '1992-05-30', 1, 'http://storage.googleapis.com/fg_merchant_image/PG/PG_diaochan_1622613681.png', 1, '016-9349794', NULL, 1, NULL, '192.168.1.1', 2, '2022-04-08 11:53:15', NULL, '2022-04-08 11:53:15', NULL, '92ca4b325023097b08ba5bd93d9862c793a6679fab5df401fd8c225539da2bcf');
+INSERT INTO `user` (`user_id`, `department_id`, `username`, `password`, `salt`, `name`, `email`, `dob`, `gender`, `avatar`, `sound`, `phone`, `remark`, `status`, `last_login`, `login_ip`, `fail_login`, `created_at`, `create_by`, `updated_at`, `update_by`, `access_token`) VALUES
+(1, NULL, 'cheeseong', 'a434df8389d477852e159ab794d9b637cfdbc19aefe896c887956b1214dcd1e9', 'zhCK7nORKh8Ou0uOI7K69pz28hbMjI33', 'Chee Seong', 'cheeseong.thrivetechsolution@gmail.com', '1992-05-30', 1, 'http://storage.googleapis.com/fg_merchant_image/PG/PG_diaochan_1622613681.png', 0, '016-9349794', NULL, 1, '2022-04-10 23:54:39', '127.0.0.1', 0, '2022-04-08 11:53:15', NULL, '2022-04-08 11:53:15', NULL, '9588ea9821e54bdd2a2f9e4f0b8ed88ef8374583e18fda07dd673e053743c7e0');
 
 --
 -- Indexes for dumped tables
